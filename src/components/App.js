a127990f5580915c94394c733a8746ff98498b48
Greg Healy <healyg@oregonstate.edu>
2020-05-04 22:26:52 -0400

    More footer merge changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
-<<<<<<< HEAD
-=======
 import Container from '@material-ui/core/Container';
->>>>>>> development
 import SignUp from './SignUp';
 import SignIn from './SignIn';
 import NavBar from './NavBar';
@@ -12,14 +9,6 @@ import Homepage from './Homepage';
 import { makeStyles } from '@material-ui/core/styles';
 import { CssBaseline } from '@material-ui/core';
 
-<<<<<<< HEAD
-class App extends React.Component {
-	render() {
-		return (
-			<div className='App'>
-				<BrowserRouter>
-					<NavBar />
-=======
 export default function App() {
 	const useStyles = makeStyles((theme) => ({
 		root: {
@@ -43,7 +32,6 @@ export default function App() {
 			<BrowserRouter>
 				<NavBar />
 				<Container component='main' className={classes.main}>
->>>>>>> development
 					<Route path='/' exact component={Homepage} />
 					<Route path='/register' component={SignUp} />
 					<Route path='/login' component={SignIn} />
