0c47b0651a42977f532ea44cff307eb1906ac707
Greg Healy <healyg@oregonstate.edu>
2020-05-06 11:57:34 -0400

    Added env variables for API URL

--- src/api/flaskapi.js
+++ src/api/flaskapi.js
@@ -1,9 +1,3 @@
 import axios from 'axios';
 
-let baseURL = 'http://127.0.0.1:5000';
-console.log(process.env.FLASK_API_URL);
-if (process.env.FLASK_API_URL !== undefined) {
-	baseURL = process.env.FLASK_API_URL;
-}
-
-export default axios.create({ baseURL });
+export default axios.create({ baseURL: process.env.REACT_APP_API_URL });
