72af1232b8f7e392e66723ba697139e6c09009a8
Greg Healy <healyg@oregonstate.edu>
2020-05-06 13:05:53 -0400

    Initial dashboard commit

---
+++ src/components/Dashboard.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+const Dashboard = () => {};
+
+const mapStateToProps = (state) => {};
+
+export default connect(mapStateToProps)(Dashboard);
