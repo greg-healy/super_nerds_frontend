2b91777e2da1af39ce7350bf001524faa88eca11
Greg Healy <healyg@oregonstate.edu>
2020-05-15 13:57:46 -0400

    Made placeholders for different components of dashboard

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,17 +1,45 @@
 import React from 'react';
-<<<<<<< HEAD
-import { connect } from 'react-redux';
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+import Container from '@material-ui/core/Container';
+import Typography from '@material-ui/core/Typography';
 
-const Dashboard = () => {};
-
-const mapStateToProps = (state) => {};
-
-export default connect(mapStateToProps)(Dashboard);
-=======
+const useStyles = makeStyles((theme) => ({
+	root: {
+		flexGrow: 1,
+	},
+	paper: {
+		padding: theme.spacing(2),
+		textAlign: 'left',
+	},
+}));
 
 const Dashboard = (props) => {
-	return <div>Dashboard!</div>;
+	const classes = useStyles();
+
+	return (
+		<Container>
+			<Grid container spacing={2}>
+				<Grid item xs={12} sm={4}>
+					<Paper className={classes.paper}>Transactions</Paper>
+				</Grid>
+				<Grid item xs={12} sm={4} container>
+					<Grid item xs container direction='column' spacing={2}>
+						<Grid item xs>
+							<Paper className={classes.paper}>Transfer to User</Paper>
+						</Grid>
+						<Grid item xs>
+							<Paper className={classes.paper}>Transfer from Bank</Paper>
+						</Grid>
+					</Grid>
+				</Grid>
+				<Grid item xs={12} sm={4}>
+					<Paper className={classes.paper}>Balance</Paper>
+				</Grid>
+			</Grid>
+		</Container>
+	);
 };
 
 export default Dashboard;
->>>>>>> development
