b2eceec3a94ef5350a0b8c81dd0fb7846f86d6bf
Greg Healy <healyg@oregonstate.edu>
2020-05-15 17:29:50 -0400

    Added action creator to fetch balance and reducer for user which holds balance

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,9 +2,11 @@ import { combineReducers } from 'redux';
 import { reducer as formReducer } from 'redux-form';
 import authReducer from './authReducer';
 import transactionReducer from './transactionReducer';
+import userReducer from './userReducer';
 
 export default combineReducers({
 	auth: authReducer,
 	form: formReducer,
 	transactions: transactionReducer,
+	user: userReducer,
 });
