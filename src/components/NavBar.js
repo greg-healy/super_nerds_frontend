e0ad9935b0fe7a5575b0716bbb05f2780c7e891f
Greg Healy <healyg@oregonstate.edu>
2020-05-16 13:06:38 -0400

    Removed app drawer, replaced with navbar buttons; centered navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,25 +2,31 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
+import Container from '@material-ui/core/Container';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
-import NavDrawer from './NavDrawer';
 import { connect } from 'react-redux';
 
 const useStyles = makeStyles((theme) => ({
 	root: {
 		flexGrow: 1,
+		justifyContent: 'flex-start',
 	},
 	menuButton: {
 		marginRight: theme.spacing(2),
 	},
 	title: {
 		flexGrow: 1,
+		justifyContent: 'flex-start',
 	},
-	navButton: {
+	authButton: {
 		marginRight: theme.spacing(2),
 		borderColor: '#fff',
+		color: '#fff',
+	},
+	navButton: {
+		marginLeft: theme.spacing(2),
 		color: '#fff',
 	},
 }));
@@ -32,7 +38,7 @@ const ButtonAppBar = ({ isSignedIn }) => {
 		return (
 			<Link to='/register'>
 				<Button
-					className={classes.navButton}
+					className={classes.authButton}
 					variant='outlined'
 					color='inherit'>
 					Register
@@ -41,28 +47,51 @@ const ButtonAppBar = ({ isSignedIn }) => {
 		);
 	};
 
+	const navButtons = [
+		{ path: '/summary', displayText: 'Summary' },
+		{ path: '/activity', displayText: 'Activity' },
+		{ path: '/send', displayText: 'Send' },
+		{ path: '/wallet', displayText: 'Wallet' },
+	];
+
+	const renderNavButton = (path, displayText) => {
+		return (
+			<Link to={path}>
+				<Button className={classes.navButton}>{displayText}</Button>
+			</Link>
+		);
+	};
+
+	const renderNavButtons = (navButtons) => {
+		return navButtons.map((navButton) =>
+			renderNavButton(navButton.path, navButton.displayText)
+		);
+	};
+
 	return (
 		<div className={classes.root}>
 			<AppBar position='static'>
-				<Toolbar>
-					<NavDrawer />
-					<Typography variant='h6' className={classes.title}>
-						<Link to='/'>
-							<Button className={classes.navButton} size='large'>
-								OUR APP NAME
+				<Container>
+					<Toolbar>
+						<Typography variant='h6' className={classes.title}>
+							<Link to='/'>
+								<Button className={classes.navButton} size='large'>
+									OUR APP LOGO
+								</Button>
+							</Link>
+						</Typography>
+						{isSignedIn ? renderNavButtons(navButtons) : ''}
+						<Link to={isSignedIn ? '/logout' : '/login'}>
+							<Button
+								className={classes.authButton}
+								variant='outlined'
+								color='inherit'>
+								{isSignedIn ? 'Log Out' : 'Log In'}
 							</Button>
 						</Link>
-					</Typography>
-					<Link to={isSignedIn ? '/logout' : '/login'}>
-						<Button
-							className={classes.navButton}
-							variant='outlined'
-							color='inherit'>
-							{isSignedIn ? 'Log Out' : 'Log In'}
-						</Button>
-					</Link>
-					{isSignedIn ? '' : renderRegister()}
-				</Toolbar>
+						{isSignedIn ? '' : renderRegister()}
+					</Toolbar>
+				</Container>
 			</AppBar>
 		</div>
 	);
