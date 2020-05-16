327cb8058a877f7c97b5902d62060f24c9be12c4
Greg Healy <healyg@oregonstate.edu>
2020-05-16 11:56:15 -0400

    Added headers to send form pages

--- src/components/SendConfirm.js
+++ src/components/SendConfirm.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import Button from '@material-ui/core/Button';
 
-const SendConfirm = ({ prevStep, nextStep }) => {
+const SendConfirm = ({ prevStep, nextStep, formValues }) => {
+	const { recip, amount } = formValues;
+
 	return (
 		<div>
 			<h1>Confirm</h1>
+			<p>
+				Are you sure you want to send ${amount} to ${recip}?
+			</p>
 			<Button variant='contained' color='primary' onClick={() => prevStep()}>
 				Back
 			</Button>
